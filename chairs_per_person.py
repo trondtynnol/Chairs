3311c6668f6129f220d936ba0048cb5cac73c4c2
trondtynnol <tty001@uit.no>
2020-02-11 15:30:55 +0100

    Fixed! (heh)

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,14 @@
-f = open("reservations.csv")
+from word2number import w2n
+
+f = open("reservations")
 
 for reservation in f:
     name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
+    try:
+        chairs_per_person = 50 / int(number)
+    except ValueError:
+        print("Please don't write numbers as text")
+    except ZeroDivisionError:
+        chairs_per_person = 0
+
     print("{} will get {} chairs per person".format(name, chairs_per_person))
